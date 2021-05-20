4dabb3afd6fc6b1b09942c583b12fe755bd4a290
JaCutillas <71320333+JaCutillas@users.noreply.github.com>
2021-05-20 10:06:56 +0200

    Update leerFicheroXML.js

--- leerFicheroXML.js
+++ leerFicheroXML.js
@@ -1,10 +1,12 @@
 function gestionarFicheroXML(xmlDoc){
 	let capaVacia = document.querySelector("main");
-	let libros = xmlDoc.getElementsByClassName("libro");
+	let libros = xmlDoc.querySelectorAll("libro");
 	
-for (let i = 0; i < libros.length; i++) {
+	for (let i = 0; i < libros.length; i++) {
 		capaVacia.innerHTML = "<div id='fila'>" + libros[i].textContent + "</div>";
+	
 	}
+	
 	
 }
 
