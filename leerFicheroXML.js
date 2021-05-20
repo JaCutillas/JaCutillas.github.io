f1beffe1db002a45bba1ee30c1db7428b212f7c1
JaCutillas <71320333+JaCutillas@users.noreply.github.com>
2021-05-20 10:39:49 +0200

    Update leerFicheroXML.js

--- leerFicheroXML.js
+++ leerFicheroXML.js
@@ -2,11 +2,18 @@ function gestionarFicheroXML(xmlDoc){
 	let capaVacia = document.querySelector("main");
 	let libros = xmlDoc.querySelectorAll("libro");
 	
-	for (let i = 0; i < libros.length; i++) {
-		capaVacia.innerHTML += "<div id='fila'>" + libros[i].textContent + "</div>";
-	
-	}
-	
+for (let i = 0; i < libros.length; i++) {
+	capaVacia.innerHTML += "<div class='fila'>" + 
+		"<div class = 'columna'>" + libros[i].querySelector("ISBN").textContent + "</div>" +
+		"<div class = 'columna'>" + libros[i].querySelector("titulo").textContent + "</div>" +
+		"<div class = 'columna'>" + libros[i].querySelector("nivelProfundidad").textContent + "</div>" +
+		"<div class = 'columna'>" + libros[i].querySelector("editorial").textContent + "</div>" +
+		"<div class = 'columna'>" + libros[i].querySelector("fechaPublicacion").textContent + "</div>" +
+		"<div class = 'columna'>" + libros[i].querySelector("paginaWeb").textContent + "</div>" +
+		"<div class = 'columna'>" + libros[i].querySelector("precio").textContent + "</div>" +
+	+ "</div>";
+
+}
 	
 }
 
