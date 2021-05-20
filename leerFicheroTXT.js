a2eb6441bb7d76d06102586dc76496cbce791f81
JaCutillas <71320333+JaCutillas@users.noreply.github.com>
2021-05-20 21:44:03 +0200

    Update leerFicheroTXT.js

--- leerFicheroTXT.js
+++ leerFicheroTXT.js
@@ -1,11 +1,21 @@
 
 function  gestionarFicheroTXT(txt)
 {
-  let lineas = txt.split("\n")
-  for(let i of lineas)
-    document.querySelector("div:nth-child(2)").innerHTML += "<p>" + i + "</p>"
+  let capaVacia = document.querySelector("main");
+  let lineas = txt.split("-");
+  let emisor = lineas[1];
+  let alineado = "derecho";
+  
+  for (let i = 0; i < lineas.length; i+=3){
+    if (lineas[i+1] == emisor){
+      alineado = "derecho";
+    }
+    else{
+      alineado = "izquierdo";
+    }
+    capaVacia.innerHTML += $"<div class='{alineado}'>" + lineas[i+2] + "</div>";
+  }
 }
 
-document.querySelector("div:nth-child(1)").addEventListener("mouseover",()=>{
- loadLDocA("leerFicherotxt.txt","txt");
-})
+loadLDocA("leerFicherotxt.txt","txt");
+
